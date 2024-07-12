a14a05ad14c8b1d87bd981a705dc25d089d52b7b
Ken Bailey <kgbailey92@gmail.com>
2024-07-12 16:26:41 -0500

    removed old ui - vue

--- vue/frontend2/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-// need this to make typescript happy w/ new component
-// TODO: fully figure out why this is necessary
-declare module '*/YTDL.vue' {
-    import Vue from 'vue';
-    export default Vue;
-}
-  
--- vue/frontend2/src/views/YoutubeDownload.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import YTDL from '../components/YTDL.vue'
-</script>
-
-<template>
-  <YTDL />
-</template>
